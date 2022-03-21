f62ba82e2b9b46c61313b53b6b477aaf5e3e30f8
sne <sne@users.noreply.github.com>
2022-03-21 16:40:15 +0100

    [ArrayOfArrays] Initial version

---
+++ ArrayOfArrays/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ArrayOfArrays;
+
+public class Program
+{
+    public static void Main()
+    {
+        Program p = new();
+        p.InitialVersion();
+    }
+
+    private void InitialVersion()
+    {
+        int[] entities_field = new int[31];
+        for (int i = 0; i < 31; i++)
+        {
+            entities_field[i] = i;
+        }
+        ref int neighbour = ref entities_field[2];
+        Console.WriteLine(neighbour);
+        entities_field[2] = 734;
+        Console.WriteLine(neighbour);
+
+        int[] refarray = new int[31];
+        refarray[0] = neighbour;
+        Console.WriteLine(refarray[0]);
+        entities_field[2] = 7;
+        Console.WriteLine(refarray[0]);
+
+        //output:
+        //2
+        //734
+        //734
+        //734
+    }
+}
